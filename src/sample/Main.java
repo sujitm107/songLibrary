c12c3ec1a90729e52af08ff752e61e8334f4def1
Sujit <sujit.mollet@rutgers.edu>
2020-02-16 21:06:50 -0500

    changed Title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello GitHUB");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -19,5 +19,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        //System.out.println("Hey");
     }
 }
